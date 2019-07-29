60a0a1b7f1669223ebd9fc175b63ab62c7034d70
Nazar Lesyuk(w) <pronaza@gmail.com>
2019-07-29 18:42:11 +0300

    ajax-json-topSlider-instagram are Done

--- app/js/common.js
+++ app/js/common.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 			});
 	});
 
-
+// top slider
 $("#carousel").owlCarousel({
 	loop: true,
 	margin: 0,
@@ -40,6 +40,13 @@ $("#carousel").owlCarousel({
 });
 
 
+// Video
+// const player = new Plyr('#player');
+const player = new Plyr(document.getElementById('player'));
+
+
+
+// 3 sliders
 owl_sex();
 owl_gey();
 owl_webcam();
@@ -54,13 +61,19 @@ owl_webcam();
 			// stagePadding: 30,
 			loop: true,
 			responsive:{
-				0:{
-					items: 1
-				},
-				600:{
+				320:{
 					items: 3
 				},
-				1000:{
+				546:{
+					items: 4
+				},
+				768:{
+					items: 5
+				},
+				992:{
+					items: 6
+				},
+				1140:{
 					items: 6
 				}
 			}
@@ -83,13 +96,19 @@ owl_webcam();
 			// stagePadding: 30,
 			loop: true,
 			responsive:{
-				0:{
-					items: 1
-				},
-				600:{
+				320:{
 					items: 3
 				},
-				1000:{
+				546:{
+					items: 4
+				},
+				768:{
+					items: 5
+				},
+				992:{
+					items: 6
+				},
+				1140:{
 					items: 6
 				}
 			}
@@ -111,13 +130,19 @@ owl_webcam();
 			// stagePadding: 30,
 			loop: true,
 			responsive:{
-				0:{
-					items: 1
-				},
-				600:{
+				320:{
 					items: 3
 				},
-				1000:{
+				546:{
+					items: 4
+				},
+				768:{
+					items: 5
+				},
+				992:{
+					items: 6
+				},
+				1140:{
 					items: 6
 				}
 			}
@@ -136,4 +161,134 @@ owl_webcam();
 });
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function declarations:
+// cookie
+function setCookie(cname, cvalue, exdays) {
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	var expires = "expires="+d.toUTCString();
+	document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i = 0; i < ca.length; i++) {
+	var c = ca[i];
+	while (c.charAt(0) == ' ') {
+		c = c.substring(1);
+	}
+	if (c.indexOf(name) == 0) {
+		return c.substring(name.length, c.length);
+	}
+	}
+	return ;
+}
+
+function checkCookie() {
+	var user = getCookie("username");
+	if (user != "") {
+	alert("Welcome again " + user);
+	} else {
+	user = prompt("Please enter your name:", "");
+	if (user != "" && user != null) {
+		setCookie("username", user, 365);
+	}
+	}
+}
+
+
+
+// class countDownTimer
+function CountdownTimer(elm, tl, mes){
+	this.initialize.apply(this,arguments);
+}
+CountdownTimer.prototype = {
+	initialize: function(elm, tl, mes) {
+		this.elem 	= document.getElementById(elm);
+		this.tl 	= tl;
+		this.mes 	= mes;
+	},
+	countDown: function(){
+		var timer = '';
+		var today = new Date();
+		var day   = Math.floor((this.tl-today)/(24*60*60*1000));
+		var hour  = Math.floor(((this.tl-today)%(24*60*60*1000))/(60*60*1000));
+		var min   = Math.floor(((this.tl-today)%(24*60*60*1000))/(60*1000))%60;
+		var sec   = Math.floor(((this.tl-today)%(24*60*60*1000))/1000)%60%60;
+		var me    = this;
+
+		if( ( this.tl - today ) > 0 ){
+
+			timer = '<div class="number-wrapper">\
+							<div class="line">	</div>\
+							<span class="number">'+this.addZero(hour)+'</span>\
+							<div class="caption">час</div>\
+						</div>';
+			timer += '<div class="number-wrapper">\
+							<div class="line">	</div>\
+							<span class="number">'+this.addZero(min)+'</span>\
+							<div class="caption">мин</div>\
+						</div>';
+			timer += '<div class="number-wrapper last">\
+							<div class="line">	</div>\
+							<span class="number">'+this.addZero(sec)+'</span>\
+							<div class="caption">сек</div>\
+						</div>';
+
+			this.elem.innerHTML = timer;
+
+			var tid = setTimeout( function(){
+					me.countDown();
+				}, 10);
+
+		} else {
+
+			this.elem.innerHTML = this.mes.html;
+			this.mes.userAction();
+			return;
+
+		}
+	},
+	addZero: function(num){ 
+		return ('0'+num).slice(-2); 
+	}
+}
 
--- app/js/vue-script.js
+++ app/js/vue-script.js
@@ -5,17 +5,124 @@ let vm = new Vue({
 			sex: true,
 			gey: true,
 			webcam: true
-		}
+		},
+		json: '' ,
+		topSliderDATA: '',
+		instagramDATA: '',
+		owlInit: {
+			loop: true,
+			margin: 0,
+			nav: false,
+			loop: true,
+			responsive: {
+				320:{items: 3},
+				546:{items: 4},
+				768:{items: 5},
+				992:{items: 6},
+				1140:{items: 6}
+			}
+		},
+
 	},
 	methods: {
+
 		activateTab(value){
 			for(let i in this.isActiveTabs) this.isActiveTabs[i] = false;
 			this.isActiveTabs[value] = true;
-		}
-	},
-	created(){
+		},
 
+		getJSON(url, callback){
+ 			var req = new XMLHttpRequest();
+			// req.responseType = 'json'; // not support in IE11
+			req.overrideMimeType("application/json");
+			req.open('GET', url, true);
+			req.onload  = function() { // запрос завершился
+				var result = JSON.parse(req.responseText);
+				console.log(result);
+				callback(result);
+			};
+			req.send(null);
+
+		},
+
+		separetaJSON(json){
+			console.log('json', json);
+			this.json = json;
+			this.topSliderDATA = json.top_slider;
+			console.log('topSliders', this.topSliderDATA);
+			
+			var randIndex = Math.floor(Math.random() * json.instagram.length); 
+			this.instagramDATA = json.instagram[randIndex];
+			console.log('instagramDATA', this.instagramDATA);
+
+		},
+		
+		topCarousel(){
+			$("#carousel").owlCarousel({
+				loop: true,
+				margin: 0,
+				nav: false,
+				loop: true,
+				responsive:{
+					320:{items: 5},
+					546:{items: 10},
+					768:{items: 12},
+					992:{items: 10},
+					1140:{items: 15}
+				}
+			});
+		},
+		
+		owl_sex(){
+			$("#owl_sex").owlCarousel(this.owlInit);
+			var owl = $('#owl_sex');
+			$('.owl_sex.prev').click(function() {
+				owl.trigger('prev.owl.carousel', [250]);
+			});
+			$('.owl_sex.next').click(function() {
+				owl.trigger('next.owl.carousel', [250]);
+			});
+		},
+		
+		owl_gey(){
+			$("#owl_gey").owlCarousel(this.owlInit);
+			var owl = $('#owl_gey');
+			$('.owl_sex.prev').click(function() {
+				owl.trigger('prev.owl.carousel', [250]);
+			});
+			$('.owl_sex.next').click(function() {
+				owl.trigger('next.owl.carousel', [250]);
+			});
+		},
+		
+		owl_webcam(){
+			$("#owl_webcam").owlCarousel(this.owlInit);
+			var owl = $('#owl_webcam');
+			$('.owl_sex.prev').click(function() {
+				owl.trigger('prev.owl.carousel', [250]);
+			});
+			$('.owl_sex.next').click(function() {
+				owl.trigger('next.owl.carousel', [250]);
+			});
+		}
+		
+	},
+	beforeMount(){
+		
 		// activate the tabs area
-		setTimeout(()=>{ this.activateTab('sex') }, 1500);
+		setTimeout(()=>{ this.activateTab('sex') }, 500);
+		
+		this.getJSON("http://localhost:3000/data.json", this.separetaJSON);
+	
+	},
+	mounted(){
+		
+		setTimeout(()=>{ 
+			this.topCarousel();
+		}, 500);
+		/* this.owl_sex();
+		this.owl_gey();
+		this.owl_webcam(); */
+		
 	}
 });
